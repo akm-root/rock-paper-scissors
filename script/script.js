c8b4d32735062ab0b40d815235931ac185e13683
akm-root <85133262+akm-root@users.noreply.github.com>
2023-06-01 12:06:35 +0530

    All functions implemented

--- script/script.js
+++ script/script.js
@@ -1,7 +1,64 @@
 function getComputerChoice() {
-  let objects = ["Rock", "Paper", "Scissors"];
-  console.log(Math.floor(Math.random() * objects.length));
+  let objects = ["rock", "paper", "scissors"];
   return objects[Math.floor(Math.random() * objects.length)];
 }
 
-console.log(getComputerChoice());
+function playRound() {
+  let userChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+  let computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  if (userChoice === computerChoice) {
+    return "tied";
+  } else if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "computer";
+    } else {
+      return "user";
+    }
+  } else if (userChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "user";
+    } else {
+      return "computer";
+    }
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "computer";
+    } else {
+      return "user";
+    }
+  }
+}
+
+function game() {
+  let userScore = 0;
+  let computerScore = 0;
+  let i = 0;
+  while (i < 5) {
+    let roundWinner = playRound();
+    if (roundWinner === "user") {
+      userScore++;
+    } else if (roundWinner === "computer") {
+      computerScore++;
+    }
+    i++;
+  }
+  if (userScore === computerScore) {
+    alert(`
+    User: ${userScore}
+    Computer: ${computerScore}
+    Game tied`);
+  } else if (userScore < computerScore) {
+    alert(`
+    User: ${userScore}
+    Computer: ${computerScore}
+    Computer won!`);
+  } else if (userScore > computerScore) {
+    alert(`
+    User: ${userScore}
+    Computer: ${computerScore}
+    User won!`);
+  }
+}
+
+game();
